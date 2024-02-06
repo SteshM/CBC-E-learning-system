a96a921dc7a843d9cf8088052e31f678a7cd96d2
SteshM <mbithestella0@gmail.com>
2024-02-06 10:50:45 +0300

    Created table Content

--- src/main/java/SteshM/CBC/Elearning/System/Entities/Content.java
+++ src/main/java/SteshM/CBC/Elearning/System/Entities/Content.java
@@ -1,4 +1,18 @@
 package SteshM.CBC.Elearning.System.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "content")
 public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long contentId;
+    private String contentUrl;
 }
