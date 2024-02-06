5b96c1af2092e8f19089846747df638971ac8142
SteshM <mbithestella0@gmail.com>
2024-02-06 10:42:30 +0300

    Created table Profile

--- src/main/java/SteshM/CBC/Elearning/System/Entities/Profile.java
+++ src/main/java/SteshM/CBC/Elearning/System/Entities/Profile.java
@@ -1,4 +1,17 @@
 package SteshM.CBC.Elearning.System.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "profile")
 public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long profileId;
 }
