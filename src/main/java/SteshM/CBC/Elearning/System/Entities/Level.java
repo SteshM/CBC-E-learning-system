a82ebe672b2d9affe9506637ec9583d04912f701
SteshM <mbithestella0@gmail.com>
2024-02-06 10:18:29 +0300

    Created table level

--- src/main/java/SteshM/CBC/Elearning/System/Entities/Level.java
+++ src/main/java/SteshM/CBC/Elearning/System/Entities/Level.java
@@ -1,4 +1,23 @@
 package SteshM.CBC.Elearning.System.Entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.annotation.Id;
+
+@EntityScan
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table (name = "level")
 public class Level {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int levelId;
+    private String LevelName;
+    private String description;
 }
