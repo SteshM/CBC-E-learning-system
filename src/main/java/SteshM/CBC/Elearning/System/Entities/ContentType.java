8ce9d41b4c88abf166f16b29099a04a2cb521f64
SteshM <mbithestella0@gmail.com>
2024-02-06 10:45:36 +0300

    Created table ContentType

--- src/main/java/SteshM/CBC/Elearning/System/Entities/ContentType.java
+++ src/main/java/SteshM/CBC/Elearning/System/Entities/ContentType.java
@@ -1,4 +1,18 @@
 package SteshM.CBC.Elearning.System.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "contentType")
 public class ContentType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long contentTypeId;
+    private String contentTypeName;
 }
