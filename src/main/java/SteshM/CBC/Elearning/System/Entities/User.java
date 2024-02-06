448f49c1864faedcfbad1878d1de8a9be20ff623
SteshM <mbithestella0@gmail.com>
2024-02-06 10:59:57 +0300

    Created tables User and UserType

--- src/main/java/SteshM/CBC/Elearning/System/Entities/User.java
+++ src/main/java/SteshM/CBC/Elearning/System/Entities/User.java
@@ -1,4 +1,22 @@
 package SteshM.CBC.Elearning.System.Entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
 public class User {
+    private long userId;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String dateOfBirth;
 }
--- src/main/java/SteshM/CBC/Elearning/System/Entities/UserType.java
+++ src/main/java/SteshM/CBC/Elearning/System/Entities/UserType.java
@@ -1,4 +1,17 @@
 package SteshM.CBC.Elearning.System.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "userType")
 public class UserType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userTypeId;
+    private long userTypeName;
 }
